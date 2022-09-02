de92217c50e1d0b4ad08a612aae3d823afecac1b
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-02 14:30:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, ' ')
+  }
+  static titleize(string){
+    let sentence = string.split(' ');
+    return sentence.forEach(str => this.capitalize.join(' '));
+  }
 }
+let name = Formatter.capitalize("job")
+console.log(name)
